cc4439900383a5dfcfbc0a377c3007642ca4cc66
DESKTOP-BSOJMUG\jinwoojung <jinu9561@gmail.com>
2023-07-06 18:25:50 +0900

    회원 웹에서 등록, 조회 기능 구현

---
+++ src/main/java/practice/hellospring/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package practice.hellospring.controller;
+
+public class MemberForm {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+
+
+}
