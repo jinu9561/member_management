ac6d6bcdc69202e07b5a7e595a0db8b8af73e7d2
DESKTOP-BSOJMUG\jinwoojung <jinu9561@gmail.com>
2023-07-12 22:43:08 +0900

    JPA를 이용한 DB와 연동

--- src/main/java/practice/hellospring/service/SpringConfig.java
+++ src/main/java/practice/hellospring/service/SpringConfig.java
@@ -1,20 +1,32 @@
 package practice.hellospring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//import practice.hellospring.repository.JdbcMemberRepository;
 import practice.hellospring.repository.MemberRepository;
 import practice.hellospring.repository.MemoryMemberRepository;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+//
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository() );
+        return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
     }
 }
